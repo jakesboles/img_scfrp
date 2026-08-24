c00ff55aecc9582705b636621beb509b2bc916bd
jakesboles <jake.boles@northwestern.edu>
2026-08-23 20:27:39 -0500

    Built Cell Bender environment from Thomas's, changed SLURM specs for seqtk after fastq truncation

--- seqtk/seqtk.sh
+++ seqtk/seqtk.sh
@@ -5,8 +5,8 @@
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 2
-#SBATCH --mem 8G
-#SBATCH --time 4:00:00
+#SBATCH --mem 16G
+#SBATCH --time 24:00:00
 #SBATCH --output /projects/b1169/boles/img_scfrp/logs/%x_%A_%a.log
 #SBATCH --verbose
 #SBATCH --array=1-68
