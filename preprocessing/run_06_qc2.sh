1825e3edb3ce1fca71cc0fae77545f5a06b79d1f
jakesboles <jake.boles@northwestern.edu>
2026-08-27 10:08:28 -0500

    Added 06 bash script

---
+++ preprocessing/run_06_qc2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account b1169
+#SBATCH --partition b1169
+#SBATCH --job-name 06_qc2
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node 16
+#SBATCH --mem 96G
+#SBATCH --time 12:00:00
+#SBATCH --output /projects/b1169/boles/img_scfrp/logs/%x_%j.log
+#SBATCH --verbose
+
+module load R/4.4.0
+module load hdf5/1.14.1-2-gcc-12.3.0
+
+Rscript /projects/b1169/boles/img_scfrp/preprocessing/06_qc2.R
